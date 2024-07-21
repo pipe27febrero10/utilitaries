6f2c839a89db9601376b7807855b7fb7776aa4a5
Felipe <feleteli@egresados.ubiobio.cl>
2024-07-20 20:00:45 -0400

    ✨ add mongoid generatorfunction

--- src/utils/generators.js
+++ src/utils/generators.js
@@ -1,3 +1,5 @@
+
+import { ObjectId } from "bson";
 export const generateRUC = () => {
   let ruc;
   let checksum = 10;
@@ -19,77 +21,79 @@ export const generateRUC = () => {
 };
 
 function getMod(t) {
-    var e, n;
-    e = 0;
-    n = 1;
-    while (t) {
-      n = (n + t % 10 * (9 - e++ % 6)) % 11;
-      t = Math.floor(t / 10)
-    }
-    if (n) {
-      return n - 1
-    } else {
-      return "K"
-    }
+  var e, n;
+  e = 0;
+  n = 1;
+  while (t) {
+    n = (n + (t % 10) * (9 - (e++ % 6))) % 11;
+    t = Math.floor(t / 10);
+  }
+  if (n) {
+    return n - 1;
+  } else {
+    return "K";
+  }
+}
+
+function getRandomNumber(rutOpts) {
+  var t, e, n, r;
+  r = rutOpts.minValue;
+  n = rutOpts.maxValue;
+  e = parseInt(t);
+  if (!e) {
+    return Math.floor(Math.random() * (n - r + 1)) + r;
+  } else {
+    return e;
+  }
+}
+
+export function generateRUTs(
+  numberOfRuts = 10,
+  minValue = 1e6,
+  maxValue = 4e7
+) {
+  let ruts = [];
+
+  const rutOpts = {
+    quantity: numberOfRuts,
+    minValue: minValue,
+    maxValue: maxValue,
   };
 
-  function getRandomNumber(rutOpts) {
-    var t, e, n, r;
-    r = rutOpts.minValue;
-    n = rutOpts.maxValue;
-    e = parseInt(t);
-    if (!e) {
-      return Math.floor(Math.random() * (n - r + 1)) + r
-    } else {
-      return e
-    }
-  };
-
-  export function generateRUTs(numberOfRuts=10, minValue=1e6, maxValue=4e7) {
-    let ruts = [];
-    
-    const rutOpts = {
-      quantity: numberOfRuts,
-      minValue: minValue,
-      maxValue: maxValue
-    };
-  
-    function generateRuts() {
-      var e, n, r, u, i;
-      n = [];
-      let t = 0;
-      while (t < rutOpts.quantity) {
-        i = getRandomNumber(rutOpts);
-        r = 0;
-        e = false;
-        while (r < n.length) {
-          if (n[r].number === i) {
-            e = true;
-            break
-          }
-          r++
+  function generateRuts() {
+    var e, n, r, u, i;
+    n = [];
+    let t = 0;
+    while (t < rutOpts.quantity) {
+      i = getRandomNumber(rutOpts);
+      r = 0;
+      e = false;
+      while (r < n.length) {
+        if (n[r].number === i) {
+          e = true;
+          break;
         }
-        if (!e) {
-          u = getMod(i);
-          n.push({
-            number: i,
-            mod: u
-          })
-        }
-        t++;
+        r++;
       }
-      return ruts = n
-    };
-  
-    
-  
-    
-  
-  
-    let generatedRuts = generateRuts().map(rut => `${rut.number}-${rut.mod}`)
-    if (generatedRuts.length === 1) { 
-      return generatedRuts[0];
+      if (!e) {
+        u = getMod(i);
+        n.push({
+          number: i,
+          mod: u,
+        });
+      }
+      t++;
     }
-    return generatedRuts;
+    return (ruts = n);
   }
 
+  let generatedRuts = generateRuts().map((rut) => `${rut.number}-${rut.mod}`);
+  if (generatedRuts.length === 1) {
+    return generatedRuts[0];
+  }
+  return generatedRuts;
+}
+
+export function generateMongoId() {
+    return new ObjectId();
+}
